6a4f0ae7c1c0f0be62c9302a36dd397832cc2d88
akarawatp <akarawat.p@siamkubota.co.th>
2023-02-16 16:49:39 +0700

    test

--- ServicePlanZero/servicePlan.py
+++ ServicePlanZero/servicePlan.py
@@ -6,6 +6,10 @@ import urllib
 import pandas as pd
 
 def run():
+    datetime_obj = datetime.datetime.now()
+    datequeryStr = datetime_obj.strftime("%Y-%m-%d")
+    # datequeryStr = '2023-01-15'
+
     ### Connect DB ####
     server = 'skcdwhprdmi.siamkubota.co.th'
     database =  'KIS Data'
@@ -17,46 +21,194 @@ def run():
     engine = sa.create_engine('mssql+pyodbc:///?odbc_connect=%s' % params)
     conn = engine.connect()
 
-    query = sa.text('''SELECT TOP (1000) [IOBJGUID]
-                    ,[Service Type]
-                    ,[Material Number]
-                    ,[Next Service Date]
-                    ,[Counter for Next Service]
-                    ,[Unit of Counter for Next Service]
-                    ,[Valid From (Time Stamp)]
-                    ,[Valid To (Time Stamp)]
-                    ,[Date of Last Change]
-                    ,[Status]
-                    ,[Request Date]
-                    ,[Approve Date]
-                    ,[Closed]
-                    ,[DBM Order Number]
-                    ,[Billing date for billing index and printout]
-                    ,[Customer]
-                    ,[Vehicle Identification Number (Vehicle Identification No.)]
-                    ,[Vehicle Sales Organization]
-                    ,[Sales Date]
-                    ,[Labor Value Main Type]
-                    ,[Engine Code]
-                FROM [ZEROSearchDB].[dbo].[Service_Plan] WHERE [Vehicle Identification Number (Vehicle Identification No.)] = 'KBCCZ494VM3F30232'
-            '''
+    query = sa.text("SELECT"
+                    "[Next Service Date]"
+                    ",[Counter for Next Service]"
+                    ",[Vehicle Identification Number (Vehicle Identification No.)]"
+                    ",[Labor Value Main Type]"
+                "FROM [ZEROSearchDB].[dbo].[Service_Plan]" 
+                "WHERE [Next Service Date] = '" + datequeryStr + "'"
             )
     resultsetloc = conn.execute(query)
     results_as_dict_loc = resultsetloc.mappings().all()
+    df = pd.DataFrame(results_as_dict_loc)
 
-    url = 'https://api.line.me/v2/bot/message/push'
-    headers = {'content-type': 'application/json','Authorization':'Bearer J9o+1YH2mYc/4RiFFOjgXTYqCIxT//ctqWgLjB4kyYlw8qaieSnNl42uyn/TMfk7PuWAe9S8hyL5JDIA00Vfr24Ltdq+97ds4BNk4htsAIRkiDDAVQ0PKiz2wreUTFBG4Vpv+hDtLSk1QAnu2V2pOwdB04t89/1O/w1cDnyilFU='}
-    body = {
-        "to": 'U97caf21a53b92919005e158b429c8c2b',
-        "messages": [
-            {
-            "type":"text",
-            "text":"Hello, user"
-            },
-            {
-                "type":"text",
-                "text":"May I help you?"
+    for index, row in df.iterrows():
+        ### Query Check User ###
+        qry = sa.text("SELECT PL.[Name],PL.[TaxId],PL.[UserId],IAC.[VIN],IAC.[Product Type],IAC.[Model] FROM [Line Data].[dbo].[Profile Line] PL "
+                    "INNER JOIN [CRM Data].[dbo].[ID_Address_Consent] IAC ON PL.[TaxId] = IAC.[Tax ID]"
+                    "WHERE IAC.[VIN] = '"+ row['Vehicle Identification Number (Vehicle Identification No.)'] +"'"
+                    "ORDER BY [UserId] OFFSET 0 ROWS FETCH NEXT 500 ROWS ONLY"
+                    )
+        resultsetCheck = conn.execute(qry)
+        results_as_dict_Check = resultsetCheck.mappings().all()
+        
+        df1 = pd.DataFrame(results_as_dict_Check)
+        for x,i in df1.iterrows():
+
+            url = 'https://api.line.me/v2/bot/message/push'
+            headers = {'content-type': 'application/json','Authorization':'Bearer J9o+1YH2mYc/4RiFFOjgXTYqCIxT//ctqWgLjB4kyYlw8qaieSnNl42uyn/TMfk7PuWAe9S8hyL5JDIA00Vfr24Ltdq+97ds4BNk4htsAIRkiDDAVQ0PKiz2wreUTFBG4Vpv+hDtLSk1QAnu2V2pOwdB04t89/1O/w1cDnyilFU='}
+            body = {
+                "to": i['UserId'],
+                "messages": [
+                    {
+                        "type": "flex",
+                        "altText": "Service Plan Report",
+                        "contents": {
+                    "type": "bubble",
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "แจ้งเตือนจากระบบ !!",
+                            "weight": "bold",
+                            "size": "xl"
+                        },
+                        {
+                            "type": "text",
+                            "text": "รถของคุณครบกำหนดตรวจเช็กระยะแล้ว",
+                            "align": "center",
+                            "size": "xs"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "margin": "lg",
+                            "spacing": "sm",
+                            "contents": [
+                            {
+                                "type": "box",
+                                "layout": "baseline",
+                                "spacing": "sm",
+                                "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "ผลิตภัณฑ์",
+                                    "color": "#aaaaaa",
+                                    "size": "sm",
+                                    "flex": 3
+                                },
+                                {
+                                    "type": "text",
+                                    "text": i['Product Type'],
+                                    "wrap": True,
+                                    "color": "#666666",
+                                    "size": "sm",
+                                    "flex": 5
+                                }
+                                ]
+                            },
+                            {
+                                "type": "box",
+                                "layout": "baseline",
+                                "spacing": "sm",
+                                "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "หมายเลขรถ",
+                                    "color": "#aaaaaa",
+                                    "size": "sm",
+                                    "flex": 3
+                                },
+                                {
+                                    "type": "text",
+                                    "text": row['Vehicle Identification Number (Vehicle Identification No.)'],
+                                    "wrap": True,
+                                    "color": "#666666",
+                                    "size": "sm",
+                                    "flex": 5
+                                }
+                                ]
+                            },
+                            {
+                                "type": "box",
+                                "layout": "baseline",
+                                "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "รุ่น",
+                                    "color": "#aaaaaa",
+                                    "size": "sm",
+                                    "flex": 3
+                                },
+                                {
+                                    "type": "text",
+                                    "text": row['Labor Value Main Type'],
+                                    "flex": 5,
+                                    "color": "#666666",
+                                    "size": "sm",
+                                    "wrap": True
+                                }
+                                ]
+                            },
+                            {
+                                "type": "box",
+                                "layout": "baseline",
+                                "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "ชั่วโมงรถ",
+                                    "color": "#aaaaaa",
+                                    "size": "sm",
+                                    "flex": 3
+                                },
+                                {
+                                    "type": "text",
+                                    "text": str(row['Counter for Next Service']),
+                                    "flex": 5,
+                                    "color": "#666666",
+                                    "size": "sm",
+                                    "wrap": True
+                                }
+                                ]
+                            },
+                            {
+                                "type": "box",
+                                "layout": "baseline",
+                                "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "วันที่ครบกำหนด",
+                                    "color": "#aaaaaa",
+                                    "size": "sm",
+                                    "flex": 3
+                                },
+                                {
+                                    "type": "text",
+                                    "text": str(row['Next Service Date']),
+                                    "flex": 5,
+                                    "color": "#666666",
+                                    "size": "sm",
+                                    "wrap": True
+                                }
+                                ]
+                            }
+                            ]
+                        },
+                        {
+                            "type": "separator",
+                            "margin": "md"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                            {
+                                "type": "text",
+                                "text": "กรุณากินข้าวให้ตรงเวลา",
+                                "size": "sm",
+                                "wrap": True,
+                                "align": "center"
+                            }
+                            ],
+                            "margin": "md"
+                        }
+                        ]
+                    }
+                    }
+                    }
+                ]
             }
-        ]
-    }
-    r = requests.post(url, headers=headers, json=body)
+            r = requests.post(url, headers=headers, json=body)
